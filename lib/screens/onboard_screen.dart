49dbd4fd3d5e33394f995f6f1a3ee54dcf9058dc
ShehanSulakshana <shehansulakshana01@gmail.com>
2026-01-06 11:31:20 +0530

    implemented splashscreen , added image assess

---
+++ lib/screens/onboard_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnboardScreen extends StatelessWidget {
+  const OnboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Onboard page", style: TextStyle(fontSize: 45)));
+  }
+}
